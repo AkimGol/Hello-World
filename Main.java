ead337ce4d71f885f7ead7e1031133138207f528
AkimGol <97281250+AkimGol@users.noreply.github.com>
2024-04-18 14:06:25 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // Показать в консоли сообщение Welcome to Java!
-        System.out.println("Welcome to Java!");
+        System.out.println("Halubionak");
+        System.out.println("Akim");
+        System.out.println(DateTime.Now);
     }
 }
