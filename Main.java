e6793976dbb95b9f788f304fb404c6d15cb3cb86
AkimGol <97281250+AkimGol@users.noreply.github.com>
2024-04-18 13:59:25 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Показать в консоли сообщение Welcome to Java!
+        System.out.println("Welcome to Java!");
+    }
+}
